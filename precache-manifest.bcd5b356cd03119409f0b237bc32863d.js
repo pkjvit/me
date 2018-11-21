02724f0316974f3ba0dc2486699eb45bc3b739cc
Pankaj Jangid <pkjvit@gmail.com>
2018-11-21 23:16:31 +0530

    Updates

--- precache-manifest.bcd5b356cd03119409f0b237bc32863d.js
+++ precache-manifest.bcd5b356cd03119409f0b237bc32863d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/me/static/js/runtime~main.f4ae667e.js"
   },
   {
-    "revision": "d73f19f1c1960dd35baa",
-    "url": "/me/static/js/main.d73f19f1.chunk.js"
+    "revision": "f034e9d918dfed43c017",
+    "url": "/me/static/js/main.f034e9d9.chunk.js"
   },
   {
     "revision": "4fcc43f1891c90f6b358",
     "url": "/me/static/js/1.4fcc43f1.chunk.js"
   },
   {
-    "revision": "d73f19f1c1960dd35baa",
+    "revision": "f034e9d918dfed43c017",
     "url": "/me/static/css/main.2ab10187.chunk.css"
   },
   {
-    "revision": "6bc70c9a9bcadaf5684f1ced57177039",
+    "revision": "b1578db0143fbdb940b2f7d941221df0",
     "url": "/me/index.html"
   }
 ];
