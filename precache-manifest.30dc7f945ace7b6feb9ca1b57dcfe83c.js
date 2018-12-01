830a1c8a6090ae7f75e0ceb30c61ee88e2ec58a2
Pankaj Jangid <pkjvit@gmail.com>
2018-12-01 10:33:24 +0530

    Updates

---
+++ precache-manifest.30dc7f945ace7b6feb9ca1b57dcfe83c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "3388ce9a2e88ed19a36f",
+    "url": "/me/static/js/main.3388ce9a.chunk.js"
+  },
+  {
+    "revision": "29e693ba95b194c18325",
+    "url": "/me/static/js/1.29e693ba.chunk.js"
+  },
+  {
+    "revision": "3388ce9a2e88ed19a36f",
+    "url": "/me/static/css/main.668ce622.chunk.css"
+  },
+  {
+    "revision": "cd4d7619d00c89d18684d34980e9a51c",
+    "url": "/me/index.html"
+  }
+];
